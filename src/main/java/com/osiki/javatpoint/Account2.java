9161f96ee7b9da1ab09d7f33b1449dc58bf66124
Segun Osiki <osiki.segun@gmail.com>
2022-12-28 07:58:27 +0100

    added account2 file

---
+++ src/main/java/com/osiki/javatpoint/Account2.java
@@ -0,0 +1,40 @@
+package com.osiki.javatpoint;
+
+public class Account2 {
+    private int acc_no;
+    private String accName;
+    private String email;
+    float amt;
+
+    public int getAcc_no() {
+        return acc_no;
+    }
+
+    public void setAcc_no(int acc_no) {
+        this.acc_no = acc_no;
+    }
+
+    public String getAccName() {
+        return accName;
+    }
+
+    public void setAccName(String accName) {
+        this.accName = accName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public float getAmt() {
+        return amt;
+    }
+
+    public void setAmt(float amt) {
+        this.amt = amt;
+    }
+}
