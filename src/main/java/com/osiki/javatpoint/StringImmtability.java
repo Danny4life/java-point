57ba9be67e51b50d2157d02c61d4daf14c02364c
Segun Osiki <osiki.segun@gmail.com>
2022-12-30 08:03:30 +0100

    added file

---
+++ src/main/java/com/osiki/javatpoint/StringImmtability.java
@@ -0,0 +1,10 @@
+package com.osiki.javatpoint;
+
+public class StringImmtability {
+    public static void main(String[] args) {
+        String name = "Segun";
+        name.concat("Osiki");
+
+        System.out.println(name);
+    }
+}
