f6dc8b62aa42127586b962537746ef1e7c817ac8
Segun Osiki <osiki.segun@gmail.com>
2022-12-30 10:29:34 +0100

    added file

--- src/main/java/com/osiki/javatpoint/StringImmtability.java
+++ src/main/java/com/osiki/javatpoint/StringImmtability.java
@@ -2,9 +2,25 @@ package com.osiki.javatpoint;
 
 public class StringImmtability {
     public static void main(String[] args) {
-        String name = "Segun";
-        name.concat("Osiki");
+        int num = 10;
+        int num1 = 20;
 
-        System.out.println(name);
+        char ch = 'a';
+
+        String repl = "Java is a fun, Java is cool";
+        String repl1 = repl.replace("Java", "");
+
+        String s = String.valueOf(num);
+        String s1 = String.valueOf(num1);
+
+        System.out.println(num + num1);
+        System.out.println(s + s1);
+
+        System.out.println(ch);
+        System.out.println(repl1);
+
+
+
+
     }
 }
