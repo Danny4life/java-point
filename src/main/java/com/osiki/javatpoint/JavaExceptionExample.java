c70558e331f66bbcc67e3251fbec632fb059ce8a
Segun Osiki <osiki.segun@gmail.com>
2023-01-01 09:59:25 +0100

    add files

--- src/main/java/com/osiki/javatpoint/JavaExceptionExample.java
+++ src/main/java/com/osiki/javatpoint/JavaExceptionExample.java
@@ -1,4 +1,20 @@
 package com.osiki.javatpoint;
 
 public class JavaExceptionExample {
+    public static void main(String[] args) {
+
+
+
+
+        try {
+            int data = 50/0;
+            System.out.println(data);
+
+
+        }catch (ArithmeticException e){
+            System.out.println("cant be divided by zero");
+        }
+        System.out.println("rest of the code");
+
+    }
 }
