1dde17e9190f3684fb70e46bab956cb56265f60a
Segun Osiki <osiki.segun@gmail.com>
2023-01-01 06:23:24 +0100

    add file

--- src/main/java/com/osiki/javatpoint/Regex1.java
+++ src/main/java/com/osiki/javatpoint/Regex1.java
@@ -5,11 +5,16 @@ import java.util.regex.Pattern;
 
 public class Regex1 {
     public static void main(String[] args) {
-        Pattern p = Pattern.compile(".s");
-        Matcher m = p.matcher("ss");
+        Pattern p = Pattern.compile("[1-10[21-26]]");
+        Matcher m = p.matcher("23");
         boolean b = m.matches();
 
-        System.out.println(b);
+        boolean b1 = Pattern.matches("[abc]+", "a");
+
+        boolean b2 = Pattern.matches("[a-zA-Z0-9]", "A");
+        boolean b3 = Pattern.matches("[789]{1}[0-9]{9}", "3982345123");
+
+        System.out.println(b3);
 
     }
 }
