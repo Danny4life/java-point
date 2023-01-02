211aedb04ceafb1dda4975a38d27cd6fe0fc2a85
Segun Osiki <osiki.segun@gmail.com>
2023-01-02 10:18:34 +0100

    add file

---
+++ src/main/java/com/osiki/javatpoint/TestThrow2.java
@@ -0,0 +1,24 @@
+package com.osiki.javatpoint;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class TestThrow2 {
+
+    public static void method() throws FileNotFoundException {
+        FileReader file = new FileReader("C:\\Users\\Anurati\\Desktop\\abc.txt");
+        BufferedReader fileReader = new BufferedReader(file);
+
+        throw new FileNotFoundException();
+    }
+
+    public static void main(String[] args) {
+        try {
+            method();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        System.out.println("rest of the code...");
+    }
+}
