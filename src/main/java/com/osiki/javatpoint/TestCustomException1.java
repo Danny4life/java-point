c663fb465fa65050ccf8b43155016891659eaa0f
Segun Osiki <osiki.segun@gmail.com>
2023-01-03 09:54:24 +0100

    add file

--- src/main/java/com/osiki/javatpoint/TestCustomException1.java
+++ src/main/java/com/osiki/javatpoint/TestCustomException1.java
@@ -1,4 +1,23 @@
 package com.osiki.javatpoint;
 
 public class TestCustomException1 {
+
+    static void validate(int age) throws CustomException{
+        if(age < 18){
+            throw new CustomException("you are not eligible to vote");
+        }else {
+            System.out.println("welcome to vote");
+        }
+    }
+
+    public static void main(String[] args) {
+        try{
+            validate(13);
+        }catch (CustomException ex){
+            System.out.println("caught the exception");
+            System.out.println("exception occured " + ex);
+
+        }
+        System.out.println("rest of the code...");
+    }
 }
