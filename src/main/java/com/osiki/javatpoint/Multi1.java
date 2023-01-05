417b095c1cddc0979399cae0470375937b04d6d7
Segun Osiki <osiki.segun@gmail.com>
2023-01-05 14:25:52 +0100

    add file

--- src/main/java/com/osiki/javatpoint/Multi1.java
+++ src/main/java/com/osiki/javatpoint/Multi1.java
@@ -1,4 +1,16 @@
 package com.osiki.javatpoint;
 
-public class Multi1 {
+public class Multi1 implements Runnable{
+
+
+    public void run (){
+        System.out.println("thread is running...");
+    }
+
+    public static void main(String[] args) {
+        Multi1 t1 = new Multi1();
+        Thread t2 = new Thread(t1);
+
+        t2.start();
+    }
 }
