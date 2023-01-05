ed528570153d5861bb2053c3ecff0ef0d8db0fea
Segun Osiki <osiki.segun@gmail.com>
2023-01-05 13:49:53 +0100

    implement runnable

--- src/main/java/com/osiki/javatpoint/Multi.java
+++ src/main/java/com/osiki/javatpoint/Multi.java
@@ -1,4 +1,18 @@
 package com.osiki.javatpoint;
 
 public class Multi extends Thread{
+    @Override
+    public void run() {
+        super.run();
+
+        System.out.println("thread is running");
+    }
+
+
+
+    public static void main(String[] args) {
+        Multi s1 = new Multi();
+
+        s1.start();
+    }
 }
---
+++ src/main/java/com/osiki/javatpoint/Multi1.java
@@ -0,0 +1,4 @@
+package com.osiki.javatpoint;
+
+public class Multi1 {
+}
