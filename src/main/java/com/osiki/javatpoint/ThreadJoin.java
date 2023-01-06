5912707427185eb331ee002f8a30932e92a718eb
Segun Osiki <osiki.segun@gmail.com>
2023-01-06 12:16:56 +0100

    add thread join example class

--- src/main/java/com/osiki/javatpoint/ThreadJoin.java
+++ src/main/java/com/osiki/javatpoint/ThreadJoin.java
@@ -1,4 +1,21 @@
 package com.osiki.javatpoint;
 
 public class ThreadJoin extends Thread{
+
+    @Override
+    public void run() {
+        super.run();
+
+        for(int j = 0; j < 2; j++){
+            try {
+                Thread.sleep(300);
+                System.out.println("the current thread name is " + Thread.currentThread().getName());
+            }catch (Exception e){
+                System.out.println(e);
+            }
+            System.out.println(j);
+        }
+
+
+    }
 }
