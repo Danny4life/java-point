4522390a50463616eaaa9ab8e0ae78871aa694f8
Segun Osiki <osiki.segun@gmail.com>
2023-01-06 12:28:30 +0100

    add files

--- src/main/java/com/osiki/javatpoint/ThreadJoinExample.java
+++ src/main/java/com/osiki/javatpoint/ThreadJoinExample.java
@@ -1,4 +1,27 @@
 package com.osiki.javatpoint;
 
 public class ThreadJoinExample {
+    public static void main(String[] args) {
+        ThreadJoin th1 = new ThreadJoin();
+        ThreadJoin th2 = new ThreadJoin();
+        ThreadJoin th3 = new ThreadJoin();
+
+        th1.start();
+
+        try {
+            System.out.println("the current thread name is " + Thread.currentThread().getName());
+            th1.join();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+
+        th2.start();
+        try{
+            System.out.println("the current thread name is " + Thread.currentThread().getName());
+            th2.join();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        th3.start();
+    }
 }
