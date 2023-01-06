45c0ab5c5de9bf254583d896b7a2f3638ce927c6
Segun Osiki <osiki.segun@gmail.com>
2023-01-06 16:10:09 +0100

    add files

--- src/main/java/com/osiki/javatpoint/ThreadNaming.java
+++ src/main/java/com/osiki/javatpoint/ThreadNaming.java
@@ -1,4 +1,35 @@
 package com.osiki.javatpoint;
 
 public class ThreadNaming extends Thread{
+    @Override
+    public void run() {
+        super.run();
+
+        System.out.println("running..");
+    }
+
+    public static void main(String[] args) {
+        ThreadNaming t1 = new ThreadNaming();
+        ThreadNaming t2 = new ThreadNaming();
+        ThreadNaming t3 = new ThreadNaming();
+        ThreadNaming t4 = new ThreadNaming();
+
+
+
+        System.out.println("name of t1 " + t1.getName());
+        System.out.println("name of t2 " + t2.getName());
+        System.out.println("name of t3 " + t3.getName());
+        System.out.println("name of t4 " + t4.getName());
+
+
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        t1.setName("segun thread");
+
+        System.out.println("after changing name of t1 " + t1.getName());
+    }
 }
