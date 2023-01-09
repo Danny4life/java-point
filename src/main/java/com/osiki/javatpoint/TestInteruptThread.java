a800e1de238ffcb7c60575b7a59e6b2c6dd5f8ff
Segun Osiki <osiki.segun@gmail.com>
2023-01-09 18:44:00 +0100

    add files

--- src/main/java/com/osiki/javatpoint/TestInteruptThread.java
+++ src/main/java/com/osiki/javatpoint/TestInteruptThread.java
@@ -1,4 +1,20 @@
 package com.osiki.javatpoint;
 
 public class TestInteruptThread extends Thread{
+
+    @Override
+    public void run() {
+        super.run();
+        for(int i = 1; i <=5; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        TestInteruptThread t1 = new TestInteruptThread();
+
+        t1.start();
+
+        t1.interrupt();
+    }
 }
