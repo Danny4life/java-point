ec865dd461f060302ebd64dee109a3acf7875f2a
Segun Osiki <osiki.segun@gmail.com>
2023-01-11 15:32:32 +0100

    add console class

---
+++ src/main/java/com/osiki/javatpoint/ConsoleRead.java
@@ -0,0 +1,12 @@
+package com.osiki.javatpoint;
+
+import java.io.Console;
+
+public class ConsoleRead {
+    public static void main(String[] args) {
+        Console c = System.console();
+        System.out.println("Enter your name: ");
+        String n = c.readLine();
+        System.out.println("Welcome " + n);
+    }
+}
