a11422bb27411029cd2ab132439256c3d7fa9d9b
Segun Osiki <osiki.segun@gmail.com>
2023-01-13 23:00:14 +0100

    add filewriter class

---
+++ src/main/java/com/osiki/javatpoint/FileWritteExample.java
@@ -0,0 +1,16 @@
+package com.osiki.javatpoint;
+
+import java.io.FileWriter;
+
+public class FileWritteExample {
+
+    public static void main(String[] args) {
+        try {
+            FileWriter fw = new FileWriter("text.txt");
+            fw.write("i LOVE java");
+        }catch (Exception e){
+            System.out.println(e);
+            System.out.println("success");
+        }
+    }
+}
