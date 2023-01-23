aa15b01f86cc62a120b773885bb93ae4552f9dd9
Segun Osiki <osiki.segun@gmail.com>
2023-01-23 17:53:20 +0100

    add file

--- src/main/java/com/osiki/javatpoint/URLDemo.java
+++ src/main/java/com/osiki/javatpoint/URLDemo.java
@@ -1,4 +1,19 @@
 package com.osiki.javatpoint;
 
+import java.net.URL;
+
 public class URLDemo {
+    public static void main(String[] args) {
+        try {
+
+            URL url = new URL("http://www.javatpoint.com/java-tutorial");
+            System.out.println("protocol " + url.getProtocol());
+            System.out.println("Host name " + url.getHost());
+            System.out.println("Port " + url.getPort());
+            System.out.println("File name " + url.getFile());
+        }catch (Exception e){
+            System.out.println(e);
+
+        }
+    }
 }
