c861bafa7fe0862aa22b7bb89743459830f63d92
Segun Osiki <osiki.segun@gmail.com>
2023-01-25 12:24:58 +0100

    add file

--- src/main/java/com/osiki/javatpoint/JavaArray1.java
+++ src/main/java/com/osiki/javatpoint/JavaArray1.java
@@ -13,5 +13,12 @@ public class JavaArray1 {
         for(int i = 0; i < arr.length; i++){
             System.out.println(arr[i]);
         }
+
+
+        int[] a = new int[]{1,2,3,4,5,6,7};
+
+        for(int i = 0; i < a.length; i++){
+            System.out.println(a[i]);
+        }
     }
 }
