c9204b164028df0cc9519696b4a7fb2a0feacf21
Segun Osiki <osiki.segun@gmail.com>
2023-04-08 11:10:18 +0100

    add file

---
+++ src/main/java/com/osiki/javatpoint/jenkov/Car.java
@@ -0,0 +1,17 @@
+package com.osiki.javatpoint.jenkov;
+
+public class Car {
+    private String brand;
+
+    public Car(String brand){
+        this.brand = brand;
+    }
+
+    public String getBrand(){
+        return this.brand;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
