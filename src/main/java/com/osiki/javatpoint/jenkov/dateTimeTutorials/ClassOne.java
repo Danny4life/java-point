ac6f87348e257a937de1c519d65a1693b56ee1dc
Segun Osiki <osiki.segun@gmail.com>
2023-06-07 18:17:29 +0100

    add file

--- src/main/java/com/osiki/javatpoint/jenkov/dateTimeTutorials/ClassOne.java
+++ src/main/java/com/osiki/javatpoint/jenkov/dateTimeTutorials/ClassOne.java
@@ -1,4 +1,21 @@
 package com.osiki.javatpoint.jenkov.dateTimeTutorials;
 
 public class ClassOne {
+
+    public static void main(String[] args) {
+        // time in miliseconds
+
+
+        long startTime = System.currentTimeMillis();
+        System.out.println(callOperation(3, 5));
+        long endTime = System.currentTimeMillis();
+
+        long totalTime = endTime - startTime;
+
+        System.out.println(totalTime);
+    }
+
+    public static int callOperation(int a, int b){
+        return a + b;
+    }
 }
